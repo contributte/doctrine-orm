ed0eeb7f960f5013e1e7fd970d0615c14cfa5af6
Petr Kessler <kesspess@gmail.com>
2018-04-25 23:40:16 +0200

    EntityManagerFactory: event manager is always taken from connection

--- src/EntityManagerFactory.php
+++ src/EntityManagerFactory.php
@@ -2,7 +2,6 @@
 
 namespace Nettrine\ORM;
 
-use Doctrine\Common\EventManager;
 use Doctrine\DBAL\Connection;
 use Doctrine\ORM\Configuration;
 use Doctrine\ORM\ORMException;
@@ -13,7 +12,6 @@ class EntityManagerFactory
 	/**
 	 * @param Connection $connection
 	 * @param Configuration $configuration
-	 * @param EventManager|NULL $eventManager
 	 * @param string $class
 	 * @return EntityManager
 	 * @throws ORMException
@@ -21,15 +19,11 @@ class EntityManagerFactory
 	public static function create(
 		Connection $connection,
 		Configuration $configuration,
-		?EventManager $eventManager = NULL,
 		string $class
 	): EntityManager
 	{
 		if (!$configuration->getMetadataDriverImpl()) {
 			throw ORMException::missingMappingDriverImpl();
-		}
-		if ($eventManager !== NULL && $connection->getEventManager() !== $eventManager) {
-			throw ORMException::mismatchedEventManager();
 		}
 
 		return new $class($connection, $configuration, $connection->getEventManager());
