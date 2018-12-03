aade7cfc0a6ee87a8b534da09a805e4a8772adeb
MartkCz <antik494@gmail.com>
2018-12-03 18:43:46 +0100

    EntityManagerDecorator::getRepository() pass self (#41)

--- src/EntityManagerDecorator.php
+++ src/EntityManagerDecorator.php
@@ -3,8 +3,28 @@
 namespace Nettrine\ORM;
 
 use Doctrine\ORM\Decorator\EntityManagerDecorator as DoctrineEntityManagerDecorator;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Repository\RepositoryFactory;
 
 class EntityManagerDecorator extends DoctrineEntityManagerDecorator
 {
+
+	/** @var RepositoryFactory */
+	private $repositoryFactory;
+
+	public function __construct(EntityManagerInterface $wrapped)
+	{
+		parent::__construct($wrapped);
+
+		$this->repositoryFactory = $wrapped->getConfiguration()->getRepositoryFactory();
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getRepository($className)
+	{
+		return $this->repositoryFactory->getRepository($this, $className);
+	}
 
 }
