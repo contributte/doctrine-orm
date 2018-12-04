c84832348463d89af7fd3e9ac8541d2d6ce38365
Jan Mikeš <j.mikes@me.com>
2018-12-04 21:34:36 +0100

    Added empty class + test

---
+++ src/DI/OrmXmlExtension.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types = 1);
+
+namespace Nettrine\ORM\DI;
+
+use Nette\DI\CompilerExtension;
+
+final class OrmXmlExtension extends CompilerExtension
+{
+
+}
---
+++ tests/cases/DI/OrmXmlExtensionTest.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types = 1);
+
+namespace Tests\Nettrine\ORM\Cases\DI;
+
+use Tests\Nettrine\ORM\Cases\TestCase;
+
+final class OrmXmlExtensionTest extends TestCase
+{
+
+}
