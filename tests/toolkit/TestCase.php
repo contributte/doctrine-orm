1e0a2e081426a618d3de422133029a4c69728318
Milan Felix Šulc <rkfelix@gmail.com>
2020-02-03 12:44:22 +0100

    Tests: move defined constants to class constants

--- tests/toolkit/TestCase.php
+++ tests/toolkit/TestCase.php
@@ -8,19 +8,6 @@ use PHPUnit\Framework\TestCase as PHPUnitTestCase;
 abstract class TestCase extends PHPUnitTestCase
 {
 
-	protected function setUp(): void
-	{
-		parent::setUp();
-
-		if (!defined('TEMP_PATH')) {
-			define('TEMP_PATH', __DIR__ . '/../tmp');
-		}
-
-		if (!defined('FIXTURES_PATH')) {
-			define('FIXTURES_PATH', __DIR__ . '/../fixtures');
-		}
-	}
-
 	protected function tearDown(): void
 	{
 		Mockery::close();
