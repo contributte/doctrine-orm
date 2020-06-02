e69bd5f15133e30ab95dea35699f1e8160770c54
Richard Kapička <richard@kapicka.cz>
2020-06-02 11:36:17 +0200

    Stop using deprecated Doctrine\Common\Persistence\...

--- src/DI/AbstractExtension.php
+++ src/DI/AbstractExtension.php
@@ -3,8 +3,8 @@
 namespace Nettrine\ORM\DI;
 
 use Contributte\DI\Extension\CompilerExtension;
-use Doctrine\Common\Persistence\Mapping\Driver\MappingDriverChain;
 use Doctrine\ORM\Configuration;
+use Doctrine\Persistence\Mapping\Driver\MappingDriverChain;
 use Nette\DI\Definitions\ServiceDefinition;
 use Nettrine\ORM\Exception\Logical\InvalidStateException;
 use stdClass;
