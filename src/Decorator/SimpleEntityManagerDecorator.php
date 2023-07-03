97373bf9ac09b0167481ca98e4e06650b218f904
Milan Felix Šulc <rkfelix@gmail.com>
2023-07-03 18:03:03 +0200

    Decorator: introduce SimpleEntityManagerDecorator

---
+++ src/Decorator/SimpleEntityManagerDecorator.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types = 1);
+
+namespace Nettrine\ORM\Decorator;
+
+use Doctrine\ORM\Decorator\EntityManagerDecorator;
+
+final class SimpleEntityManagerDecorator extends EntityManagerDecorator
+{
+
+}
