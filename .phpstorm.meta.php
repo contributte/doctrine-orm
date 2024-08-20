f3afa2cf045259e1f4c996a693643510656ff95c
Vít Kutný <vit@kutny.cz>
2024-08-20 11:04:07 +0200

    Remove wrong overrides from .phpstorm.meta.php
    Overrides for getRepository are invalid – it overrides returned type from `@returns Collection<T>` to returning the entity directly `@returns T`
    
    See https://github.com/DEVSENSE/phptools-docs/issues/620

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,13 +5,7 @@ namespace PHPSTORM_META {
 	override(\Doctrine\ORM\EntityManagerInterface::find(0), map([
 		'' => '@',
 	]));
-	override(\Doctrine\ORM\EntityManagerInterface::getRepository(0), map([
-		'' => '@',
-	]));
 	override(\Doctrine\ORM\EntityManagerInterface::getReference(0), map([
-		'' => '@',
-	]));
-	override(\Doctrine\Persistence\ObjectManager::getRepository(0), map([
 		'' => '@',
 	]));
 }
